527568de83c3d1fed3453a1579ea4737d2d7e254
Joana M. F. da Trindade <jmftrindade@gmail.com>
2021-07-30 11:56:20 -0400

    use sysctl instead

--- load_vtune.sh
+++ load_vtune.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 echo "Enabling ptrace level 0..."
-echo "0" | sudo tee /proc/sys/kernel/yama/ptrace_scope
+sudo sysctl -w kernel.yama.ptrace_scope=0
+echo "Confirming it's set (below should print 0):"
+sudo cat /proc/sys/kernel/yama/ptrace_scope
+
+echo "\nEnabling kallsyms kernel module tracing level 0..."
+sudo sysctl -w kernel.kptr_restrict=0
+echo "Confirming it's set (below should print 0):"
+sudo cat /proc/sys/kernel/kptr_restrict
 
 echo "Loading VTune modules..."
-. $VTUNE_HOME/sepdk/src/insmod-sep
+sudo sh $VTUNE_HOME/sepdk/src/insmod-sep
